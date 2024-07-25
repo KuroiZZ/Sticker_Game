6a07371fd7bdd6f8b5711be1724af16e3de0a966
Habil Tatarogullari <habiltatar01@gmail.com>
2024-07-25 13:39:12 +0300

    paper sizing animation added.

--- Assets/codes/SlideButton_sc.cs
+++ Assets/codes/SlideButton_sc.cs
@@ -5,20 +5,29 @@ using UnityEngine.EventSystems;
 
 public class SlideButton : MonoBehaviour, IPointerClickHandler
 {
+    GameObject Canvas;
     GameObject Menu;
-
+    GameObject Paper;
     Animator Menu_Animator;
+    Animator Paper_Animator;
     // Start is called before the first frame update
     void Start()
     {
-        Menu = gameObject.transform.parent.gameObject;
+        Canvas = gameObject.transform.parent.parent.gameObject;
+        Paper = Canvas.transform.GetChild(0).gameObject;
+        Menu = Canvas.transform.GetChild(1).gameObject;
         Menu_Animator = Menu.GetComponent<Animator>();   
+        Paper_Animator = Paper.GetComponent<Animator>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         bool isShown = Menu_Animator.GetBool("isShown");
+
+        bool isSmalled = Paper_Animator.GetBool("isSmalled");
         
-        Menu_Animator.SetBool("isShown", !isShown);
+        Menu_Animator.SetBool("isShown", !isShown); 
+        Paper_Animator.SetBool("isSmalled", !isSmalled);
+
     }
 }
